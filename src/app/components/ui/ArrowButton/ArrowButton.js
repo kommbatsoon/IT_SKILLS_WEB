960c24a991097fa97540d8566ba0b422d9ba858c
Pavel Unhur <pavel.unhur@gmail.com>
2020-07-12 22:42:57 +0300

    Fix ArrowButton styles

--- src/app/components/ui/ArrowButton/ArrowButton.js
+++ src/app/components/ui/ArrowButton/ArrowButton.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 import {Icon} from '@app/components';
 import {APP_ICON} from '@util/constants';
 
+import './ArrowButton.scss';
+
 export const ArrowButton = (props) => {
 	const {onClick} = props;
 
 	return (
-		<button className='button' onClick={onClick}>
+		<button className='arrowButton' onClick={onClick}>
 			<span className='text'>Подробнее</span>
 			<Icon className='whiteIcon' name={APP_ICON.rightArrowWhite} />
 			<Icon className='blackIcon' name={APP_ICON.rightArrowBlack} />
