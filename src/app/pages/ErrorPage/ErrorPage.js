178565ef646a560faed0d2632dd30e813c5b273d
Pavel Unhur <pavel.unhur@gmail.com>
2020-07-16 21:40:23 +0300

    Implement Error page

--- src/app/pages/ErrorPage/ErrorPage.js
+++ src/app/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export const ErrorPage = () => {
+import {Button} from '@app/components';
+
+import style from './ErrorPage.scss';
+
+const DEFAULT_CONFIG = {
+	title: '404',
+	description: 'Страница не найдена',
+	buttonText: 'Вернуться на главную',
+};
+
+export const ErrorPage = ({config = DEFAULT_CONFIG}) => {
 	return (
-		<div className='page-wrapper'>
-			<h1>404 error</h1>
+		<div className={style.wrapper}>
+			<p className={style.errorTitle}>{config.title}</p>
+			<p className={style.errorDescription}>{config.description}</p>
+			<Button>{config.buttonText}</Button>
 		</div>
 	);
 };
